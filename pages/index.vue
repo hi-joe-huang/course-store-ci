094327faca72b9a0592072af76d6907f48dc0c89
Riceball <42161586+Anderice@users.noreply.github.com>
2023-08-24 21:06:11 +0800

    Init

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+
+    <Tutorial />
+    <h1>test</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage',
+})
+</script>
