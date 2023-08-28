c5cac96010e4cf18502c85029ae0e14effa1becd
Riceball <42161586+Anderice@users.noreply.github.com>
2023-08-29 00:57:30 +0800

    feat: Add primary-light color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,7 @@ export default <Partial<Config>>{
       colors: {
         'global-background': 'hsl(var(--color-global-background) / <alpha-value>)',
         'primary': 'hsl(var(--color-primary) / <alpha-value>)',
+        'primary-light': 'hsl(var(--color-primary-light) / <alpha-value>)',
         'accent':  'hsl(var(--color-accent) / <alpha-value>)',
         'accent-light':  'hsl(var(--color-accent-light) / <alpha-value>)'
       }
