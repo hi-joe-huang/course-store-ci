1c6d220602d31ded575c622ab45ff5dc87e2f6b6
Riceball <42161586+Anderice@users.noreply.github.com>
2023-08-29 19:09:58 +0800

    feat: Add default layout

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="bg-global-background">
+    <global-navbar/>
+      <div class="container mx-auto px-4 min-h-screen">
+        <Nuxt />
+      </div>
+    <global-footer/>
+    <global-modal-login v-if="modalStore.isLoginModalOpen" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { useModalStore } from '@/stores/modal'
+const modalStore = useModalStore()
+</script>
