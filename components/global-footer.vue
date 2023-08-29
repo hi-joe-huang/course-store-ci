fbb72dfbc4ebb67c9999370c970aac634bc7f5d2
Riceball <42161586+Anderice@users.noreply.github.com>
2023-08-29 19:14:41 +0800

    feat: Add footer

---
+++ components/global-footer.vue
@@ -0,0 +1,5 @@
+<template>
+  <footer class="text-center mx-auto py-4">
+    網頁由 <a class="underline" href="https://weweweb.pages.dev/">黃宗瑋</a> 製作，僅用於 <a href="https://hiskio.com" class="underline">HiSKIO</a> 面試
+  </footer>
+</template>
